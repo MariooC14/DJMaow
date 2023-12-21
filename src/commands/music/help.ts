2b20d03ae54056e77bc727b536627b5c9a347d3a
Mario <cavalmario2003@gmail.com>
2023-12-21 21:30:43 +0000

    Added /help command

--- src/commands/music/help.ts
+++ src/commands/music/help.ts
@@ -1,12 +1,15 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { Command } from '../../types';
+import fs from 'fs';
+
+const commandDescriptions = JSON.parse(fs.readFileSync('src/assets/command_overview.json', 'utf-8'));
+
+type commandName = 'play'| 'pause' | 'resume' | 'skip' | 'stop' | 'queue' | 'clear' | 'skipto' | 'fuckyoudavid'| null
 
 export const helpCommand: Command = {
-  command: new SlashCommandBuilder()
-    .setName('help')
-    .setDescription('Command guide')
+  command: new SlashCommandBuilder().setName('help').setDescription('Command guide')
     .addStringOption(option =>
-      option.setName('command')
+      option.setName('command').setDescription('The command you need help with')
         .addChoices(
           { name: 'play', value: 'play' },
           { name: 'pause', value: 'pause' },
@@ -17,13 +20,17 @@ export const helpCommand: Command = {
           { name: 'clear', value: 'clear' },
           { name: 'skipto', value: 'skipto' },
           { name: 'fuckyoudavid', value: 'fuckyoudavid' },
-          { name: 'help', value: 'help' },
         ),
     ),
   execute: async (interaction) => {
-    const command = interaction.options.getString('command');
-    console.log(command);
-    console.log('hi');
+    const command = interaction.options.getString('command') as commandName;
 
+    // Reply with an overview of the commands.
+    if (!command) {
+      await interaction.reply(commandDescriptions['overview'].description);
+    }
+    else {
+      await interaction.reply(commandDescriptions[command].description);
+    }
   },
 };
